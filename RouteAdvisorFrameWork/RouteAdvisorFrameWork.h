98cb0b366fe3fc2d7a0410b09702eb2d7056fb5b
cbalufo <cbalufo@dync-34-170.ac.upc.edu>
2015-04-29 15:42:14 +0200

    Initial Commit

---
+++ RouteAdvisorFrameWork/RouteAdvisorFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  RouteAdvisorFrameWork.h
+//  RouteAdvisorFrameWork
+//
+//  Created by cbalufo on 29/4/15.
+//  Copyright (c) 2015 DAMA-UPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RouteAdvisorFrameWork.
+FOUNDATION_EXPORT double RouteAdvisorFrameWorkVersionNumber;
+
+//! Project version string for RouteAdvisorFrameWork.
+FOUNDATION_EXPORT const unsigned char RouteAdvisorFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RouteAdvisorFrameWork/PublicHeader.h>
+
+
